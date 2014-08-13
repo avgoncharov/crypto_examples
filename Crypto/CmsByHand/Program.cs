4ac376c764bc9466aee6d80eba874a3535118ebb
Andrey Goncharov <a.v.goncharov@voskhod.ru>
2014-08-13 14:34:07 +0400

    Add project which show how cms can be created by using BouncyCastle.

---
+++ Crypto/CmsByHand/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * User: Andrey Goncharov.
+ * Date: 2014-08-13. 
+ */
+using System;
+
+namespace CmsByHand
+{
+	/// <summary>
+	/// Program create CMS container by using BouncyCastle.
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
